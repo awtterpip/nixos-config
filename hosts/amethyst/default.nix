754ff43d5c64f8acacf9d7f486eb07814e023429
awtterpip <awtterpip@gmail.com>
2024-08-27 02:29:14 -0500

    openrgb

--- hosts/amethyst/default.nix
+++ hosts/amethyst/default.nix
@@ -1,4 +1,7 @@
-{...}: {
+{pkgs, ...}: {
   imports = [(import ./../shared/amd.nix) (import ./hardware-configuration.nix)];
   networking.hostName = "amethyst";
+  # openrgb
+  services.hardware.openrgb.enable = true;
+  environment.systemPackages = with pkgs; [openrgb-with-all-plugins];
 }
